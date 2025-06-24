8e422ced009ee758220f8039be3815366ffb41d2
momoo17 <momo17@ymobile.ne.jp>
2025-06-25 04:24:51 +0900

    edit

--- app/components/common/Bubbles.tsx
+++ app/components/common/Bubbles.tsx
@@ -97,8 +97,8 @@ export default function Bubbles({
           x: Math.random() * width,
           y: Math.random() * height,
           r: Math.random() * 75 + 200, // サイズのランダム化
-          vx: Math.random() * 2 - 0.5,
-          vy: Math.random() * 2 - 0.5
+          vx: Math.random() * 1 - 0.25,
+          vy: Math.random() * 1 - 0.25
         });
       }
       ballsRef.current = balls;
